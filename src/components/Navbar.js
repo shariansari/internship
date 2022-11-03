af7d7241ebdbd9bbb5eb86c28559b1a11e94e4a9
unknown <faizanansari6882@gmail.com>
2022-11-03 23:46:08 +0530

    second

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
-        <nav>
-        
-            <Link to="/">Top Performer</Link>
-            <Link to="/Weak">Performer Of The Weak</Link>
-            <Link to="/Month">Performer Of The Month</Link>
-        
-        </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ">
+            <Link className="nav-item nav-link active" to="/">Top Performer Of The Day</Link>
+            <Link className="nav-item nav-link" to="/weak">Top Performer Of This Week</Link>
+            <Link className="nav-item nav-link " to="/Month">Top Performer Of This Month</Link>
+          </div>
+        </div>
+      </nav>
     
     </div>
   )
--- src/components/pages/Top.js
+++ src/components/pages/Top.js
@@ -2,55 +2,61 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 function Top() {
-  const Cardinfo = [
+  const cardInfo = [
     {
-      image: "https:/source.unsplash.com/random/?boy",
+      image: "https:/source.unsplash.com/random/200x200/?boy",
       title: "Akash",
       text: "he is good",
     },
     {
-      image: "https:/source.unsplash.com/random/?girl",
+      image: "https:/source.unsplash.com/random/200x200/?girl",
       title: "Anshika",
       text: "he is second good",
     },
     {
-      image: "https:/source.unsplash.com/random/?lady",
+      image: "https:/source.unsplash.com/random/200x200/?lady",
       title: "lavina",
       text: "he is third good",
     },
     {
-      image: "https:/source.unsplash.com/random/?Men",
+      image: "https:/source.unsplash.com/random/200x200/?Men",
       title: "Noor",
       text: "he is forth good",
     },
     {
-      image: "https:/source.unsplash.com/random/?teacher",
+      image: "https:/source.unsplash.com/random/200x200/?teacher",
       title: "Rubika",
       text: "he is sixth good",
     },
     {
-      image: "https:/source.unsplash.com/random/?Moms",
+      image: "https:/source.unsplash.com/random/200x200/?Moms",
+      title: "lavanya",
+      text: "he is fifth good",
+    },
+    {
+      image: "https:/source.unsplash.com/random/200x200/?toy",
+      title: "lavanya",
+      text: "he is fifth good",
+    },
+    {
+      image: "https:/source.unsplash.com/random/200x200/?fish",
       title: "lavanya",
       text: "he is fifth good",
     },
   ];
-  console.log(Cardinfo,"shariq")
- 
-  const rendercards = (card ,index)=>{
-<Card style={{ width: "18rem" }}> key={index}
-        <Card.Img variant="top" src={card.image} />
-        <Card.Body>
-          <Card.Title>{card.title}</Card.Title>
-          <Card.Text>
-            {card.text}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-  }
-  return (
+  console.log(cardInfo, "shariq");
 
-    <div>
-      {Cardinfo.map(rendercards)}
+  return (
+    <div className="row">
+   {cardInfo.map((item,i) => {
+       return  <div className="col-3 mt-3" > <Card  style={{ width: "18rem" }} key={i}>
+          <Card.Img  variant="top" src={item.image} />
+          <Card.Body  >
+            <Card.Title  >{item.title}</Card.Title>
+            <Card.Text>{item.text}</Card.Text>
+          </Card.Body>
+        </Card></div>
+      })}
     </div>
   );
 }
